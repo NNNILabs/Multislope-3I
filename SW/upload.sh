20bccec86229473ebca70eb0cfe2b4e37a2f74d6
diminDDL <dimin21590@gmail.com>
2022-07-12 16:14:09 +0200

    updated upload script

--- SW/upload.sh
+++ SW/upload.sh
@@ -1,4 +1,18 @@
 cd ./build/
 make
 cd ..
-cp ./build/Multislope.uf2 /media/$USER/RPI-RP2/
+name=$(find ./build/ -name "*.uf2")
+echo "found: $name"
+RPI="/media/$USER/RPI-RP2/"
+echo "waiting for RPI..."
+timeout=0
+while [ ! -d "$RPI" ]; do
+    sleep 1
+    timeout=$((timeout+1))
+    if [ $timeout -gt 10 ]; then
+        echo "RPI not found"
+        exit 1
+    fi
+done
+echo "found $RPI"
+cp $name /media/$USER/RPI-RP2/
