c0746eaaa0b1249d0e7668d35ae3299c20f7dfb3
NNNILabs <osvivekanand002@gmail.com>
2024-07-05 00:43:13 +0200

    updated files

--- SW/main.c
+++ SW/main.c
@@ -15,7 +15,7 @@
 #define MAINS_FREQ 50                   // Hz
 
 const float div = 20;
-uint32_t pwmCycles = 6000;
+uint32_t pwmCycles = 600000;
 
 const uint8_t MUX_A0 = 0;
 const uint8_t MUX_A1 = 2;
@@ -30,19 +30,15 @@ PIO pio;
 
 uint multislopeSM;
 uint residueSM;
+uint calibrationSM;
 
 const uint32_t residueConfig = 53248;
 
-uint32_t counts = 0;
-uint32_t residueBefore = 0;
-uint32_t residueAfter = 0;
-uint32_t residueCal = 0;
-
-#if MAINS_FREQ == 50
-    const uint32_t MScyclesPerPLC = 6000;
-#elif MAINS_FREQ == 60
-    const uint32_t MScyclesPerPLC = 5000;
-#endif
+int32_t counts = 0;
+int32_t residueBefore = 0;
+int32_t residueAfter = 0;
+int32_t RUU = 0;
+int32_t RUD = 0;
 
 // MCP3202 ADC
 #define CS   13
@@ -102,10 +98,56 @@ void get_counts(uint32_t countNum)
     gpio_put(PICO_DEFAULT_LED_PIN, false);
 }
 
+void get_cal()
+{
+    gpio_put(PICO_DEFAULT_LED_PIN, true);
+
+    uint32_t countOne = 0;
+    uint32_t countTwo = 0;
+    uint32_t countThree = 0;
+    uint32_t countFour = 0;
+    while(countFour == 0)
+    {
+        pio_sm_put_blocking(pio, calibrationSM, 1);
+        sleep_us(15);
+        pio_sm_put_blocking(pio, residueSM, (residueConfig << 16));
+        countOne = pio_sm_get_blocking(pio, residueSM);
+        sleep_us(15);
+        pio_sm_put_blocking(pio, residueSM, (residueConfig << 16));
+        countTwo = pio_sm_get_blocking(pio, residueSM);
+        sleep_us(15);
+        pio_sm_put_blocking(pio, residueSM, (residueConfig << 16));
+        countThree = pio_sm_get_blocking(pio, residueSM);
+        sleep_us(15);
+        pio_sm_put_blocking(pio, residueSM, (residueConfig << 16));
+        countFour = pio_sm_get_blocking(pio, residueSM);
+        sleep_ms(1);
+    }
+
+    pio_sm_clear_fifos(pio, calibrationSM);
+    pio_sm_clear_fifos(pio, residueSM);
+
+    uint32_t R1 = (countTwo - countOne);
+    uint32_t R2 = (countThree - countTwo);
+    uint32_t R3 = (countFour - countThree);
+    
+    RUD = (R2 > R1)? (R2 - R1) : (R1 - R2);
+    RUU = (R3 > R2)? (R3 - R2) : (R2 - R3);
+
+    // printf("%d, %d, %d, %d\n", countOne, countTwo, countThree, countFour);
+    // printf("%d, %d, %d\n", R1, R2, R3);
+    // printf("%d, %d\n", RUD, RUU);
+
+    gpio_put(PICO_DEFAULT_LED_PIN, false);
+
+}
+
 int main() 
 {
     set_sys_clock_khz(96000, true);  
     stdio_init_all();
+
+    sleep_ms(1000);
 
     gpio_init(PICO_DEFAULT_LED_PIN);
     gpio_set_dir(PICO_DEFAULT_LED_PIN, GPIO_OUT);
@@ -127,66 +169,75 @@ int main()
     gpio_set_dir(13, GPIO_OUT);
     gpio_put(13, false);
 
-    // setMuxState(MUX_RAW);
-    // setMuxState(MUX_RAW);
-
-    // initialise multislope PIO
     pio = pio0;
-    multislopeSM = pio_claim_unused_sm(pio, true);
-    uint multislopeOffset = pio_add_program(pio, &ms_program);
 
     residueSM = pio_claim_unused_sm(pio, true);
     uint residueOffset = pio_add_program(pio, &residue_program);
 
+    calibrationSM = pio_claim_unused_sm(pio, true);
+    uint calibrationOffset = pio_add_program(pio, &calibration_program);
+
+    residue_program_init(pio, residueSM, residueOffset, MISO, div);
+    calibration_program_init(pio, calibrationSM, calibrationOffset, PWMA, COMP, div, MEAS);
+
+    pio_sm_clear_fifos(pio, residueSM);
+    pio_sm_clear_fifos(pio, calibrationSM);
+
+    pio_sm_set_enabled(pio, residueSM, true);
+    pio_sm_set_enabled(pio, calibrationSM, true);
+
+    get_cal();
+    get_cal();
+
+    pio_sm_set_enabled(pio, residueSM, false);
+    pio_sm_set_enabled(pio, calibrationSM, false);
+
+    pio_remove_program(pio, &calibration_program, calibrationOffset);
+    pio_remove_program(pio, &residue_program, residueOffset);
+
+    multislopeSM = pio_claim_unused_sm(pio, true);
+    uint multislopeOffset = pio_add_program(pio, &ms_program);
+
+    residueSM = pio_claim_unused_sm(pio, true);
+    residueOffset = pio_add_program(pio, &residue_program);
+
     ms_program_init(pio, multislopeSM, multislopeOffset, PWMA, COMP, div, MEAS);
     residue_program_init(pio, residueSM, residueOffset, MISO, div);
 
-    // Start running our PIO program in the state machine
     pio_sm_set_enabled(pio, multislopeSM, true);
-
     pio_sm_set_enabled(pio, residueSM, true);
 
-    get_counts(12); // start dithering
+    get_counts(50);
 
     int chr = 0;
 
     uint32_t newInput = 0;
     char inputBuffer[32] = {0};
 
+    setMuxState(MUX_GND);
+    sleep_us(10);
+    get_counts(pwmCycles);
+    int32_t finalGround = (counts * RUU) - ((6000 - counts) * RUD) + (residueAfter - residueBefore);
 
+    setMuxState(MUX_RAW);
+    sleep_us(10);
+    get_counts(pwmCycles);
+    int32_t finalRaw = (counts * RUU) - ((6000 - counts) * RUD) + (residueAfter - residueBefore);
+
+    setMuxState(MUX_GND);
 
     while(true)
     {
         // sleep_ms(500);
         newInput = scanf("%s", &inputBuffer, 31);         // Read input from serial port
 
-        // calibration of residue ADC
-        setMuxState(MUX_GND);
-        sleep_us(100);
-        get_counts(3);
-        residueCal = (residueAfter > residueBefore) ? (residueAfter - residueBefore) : (residueBefore - residueAfter);
-
-        setMuxState(MUX_GND);
-        sleep_us(100);
+        sleep_us(10);
         get_counts(pwmCycles);
-        uint32_t countGnd = ((residueCal * ((2 * counts) - pwmCycles)) + (residueAfter - residueBefore));
-        sleep_us(100);
+        int32_t finalIn = (counts * RUU) - ((6000 - counts) * RUD) + (residueAfter - residueBefore);
 
-        setMuxState(MUX_RAW);
-        sleep_us(100);
-        get_counts(pwmCycles);
-        uint32_t countRaw = ((residueCal * ((2 * counts) - pwmCycles)) + (residueAfter - residueBefore));
-        sleep_us(100);
+        double voltage = 6.85f * (double)(finalIn - finalGround)/(double)(finalRaw - finalGround);
 
-        setMuxState(MUX_IN);
-        sleep_us(100);
-        get_counts(pwmCycles);
-        uint32_t countIn = ((residueCal * ((2 * counts) - pwmCycles)) + (residueAfter - residueBefore));
-        sleep_us(100);
-
-        setMuxState(MUX_GND);
-
-        printf("%d, %d, %d\n", countGnd, countRaw, countIn);
+        printf("%d, %d, %d, %012lf\n", finalGround, finalRaw, finalIn, voltage);
     }
     
     return 0;
